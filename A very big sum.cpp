a3cf79407142776c84afb96bad181a2781216ec5
Ishaan Chaturvedi <ishaan27chaturvedi@gmail.com>
2016-01-29 00:29:56 +0530

    First ommit

---
+++ A very big sum.cpp
@@ -0,0 +1,24 @@
+//A very big sum
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    long long int sum =0, a[n];
+    int i=n;
+
+    while(i)
+    {
+            cin>>a[i];
+            sum=sum+a[i];
+            i--;
+
+    }
+
+    cout<<sum;
+}
